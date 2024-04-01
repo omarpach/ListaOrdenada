bf31a37e27a6df1e856226b28e700d89e6d890f6
OmarP-hub <omy250@gmail.com>
2024-04-01 10:21:56 -0700

    Metodos para el buen funcionamiento de la clase

--- ListaOrdenada.hpp
+++ ListaOrdenada.hpp
@@ -8,8 +8,8 @@ private:
 public:
   ListaOrdenada();
   ~ListaOrdenada();
-  ListaOrdenada<T>(const ListaOrdenada<T> &lista2);
-  ListaOrdenada<T> &operator=(const ListaOrdenada<T> &lista2) const;
+  ListaOrdenada<T>(const ListaOrdenada<T> &lista1);
+  ListaOrdenada<T> &operator=(const ListaOrdenada<T> &lista1);
   void Agregar(T valor);
   void Eliminar(T valor);
   bool BuscarElemento(T valor) const;
@@ -20,3 +20,17 @@ public:
   void ImprimirDescendente() const;
   ListaOrdenada<T> Mezclar(const ListaOrdenada<T> &lista1) const;
 };
+
+template <typename T> ListaOrdenada<T>::ListaOrdenada() {}
+
+template <typename T> ListaOrdenada<T>::~ListaOrdenada() { lista.Vaciar(); }
+
+template <typename T>
+ListaOrdenada<T>::ListaOrdenada(const ListaOrdenada<T> &lista1) {
+  *this = lista1;
+}
+
+template <typename T>
+ListaOrdenada<T> &ListaOrdenada<T>::operator=(const ListaOrdenada<T> &lista1) {
+  this->lista = lista1.lista;
+}
